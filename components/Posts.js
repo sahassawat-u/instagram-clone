b9a99d0d61ca99eed6661a8e529cf27a6aa81daa
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-02 19:21:46 +0700

    finished instagram build

--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,44 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post"
 
-const posts = [
-    {
-        id:'123',
-        username: 'hhhharyyy',
-        userImg: "https://links.papareact.com/3ke",
-        img: "https://links.papareact.com/3ke",
-        caption: "YEAHH I AM F***ING TOMBOY",
-    }
-]
+// const posts = [
+//     {
+//         id:'123',
+//         username: 'hhhharyyy',
+//         userImg: "https://links.papareact.com/3ke",
+//         img: "https://links.papareact.com/3ke",
+//         caption: "YEAHH I AM F***ING TOMBOY",
+//     }
+// ]
 
 function Posts() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp','desc')), snapshot => {
+      setPosts(snapshot.docs);
+    });
+
+    // return () => {
+    //   unSubscribe();
+    // }
+
+  }, [db]);  
+
   return (
     <div>  
         {posts.map(post => (
             <Post key={post.id}
             id={post.id}
-            username={post.username}
-            userImg={post.userImg}
-            img={post.img}
-            caption={post.caption}
+            username={post.data().username}
+            userImg={post.data().profileImg}
+            img={post.data().image}
+            caption={post.data().caption}
             />
         ))}
-        
-
-            {/* Posts */}
-            {/* Posts */}
-            {/* Posts */}
-            {/* Posts */}
     </div>
   )
 }
